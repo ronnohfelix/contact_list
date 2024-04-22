a1195de2b6261e14d3b924f0da27b8d882ab352e
Felix Kipchirchir Ronoh <felixronoh258@gmail.com>
2024-04-23 00:49:01 +0300

    contacts created

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+from flask_cors import CORS # remove cors error while fetching data from frontend
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' # database path
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # no modification tracking
 
-db = SQLAlchemy(app)
+db = SQLAlchemy(app) # database object
